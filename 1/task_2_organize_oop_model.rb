04889fd06a7731d19027d48b4c02d7ec55a44e8e
illia108 <illia@ukr.net>
2016-11-03 13:19:09 +0200

    Organize oop model

---
+++ 1/task_2_organize_oop_model.rb
@@ -0,0 +1,26 @@
+class NamedObject
+  attr_reader :name
+
+  def initialize name
+   @name = name 
+  end
+end
+
+class Human < NamedObject
+  def invite invitee
+    invitee
+  end
+
+  def to_drink drink
+    "#{@name} agrees to drink some #{drink.name} with you!"
+  end
+end
+
+class Drink < NamedObject
+end
+
+volodya = Human.new('Volodya')
+ira = Human.new('Ira')
+coffee = Drink.new('Latte')
+
+puts volodya.invite(ira).to_drink coffee
